6ce36e50ba7a9ccbc07d61f3c2be8c2b95055480
Evan Yip <etyip@ucsb.edu>
2019-10-15 10:05:36 -0700

    Updates

---
+++ precache-manifest.828d69b9b3ddfb4a9632f4bffb9d42c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb988b5283bf76202845fb28b7c644b",
+    "url": "/portfolio_fall_2019/index.html"
+  },
+  {
+    "revision": "15b010fd63825f423236",
+    "url": "/portfolio_fall_2019/static/js/2.0d58de2b.chunk.js"
+  },
+  {
+    "revision": "adb8e087cb6b7e6abc9e",
+    "url": "/portfolio_fall_2019/static/js/main.bd501aa3.chunk.js"
+  },
+  {
+    "revision": "84995aae981a3f658e5c",
+    "url": "/portfolio_fall_2019/static/js/runtime-main.9df8533a.js"
+  },
+  {
+    "revision": "e1f3fa144adeb4da06e61350f2d66a5f",
+    "url": "/portfolio_fall_2019/static/media/gotfeedback.e1f3fa14.png"
+  }
+]);
